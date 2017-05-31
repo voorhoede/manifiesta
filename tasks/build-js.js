46eea98605c3ef7795d3d614d5079d6308e73ab0
Dylan van Zanten <info@dylanvanzanten.nl>
2017-05-31 16:38:00 +0200

    Changed classes in svg inside top-bar.

--- tasks/build-js.js
+++ tasks/build-js.js
@@ -3,14 +3,18 @@ const concat = require('gulp-concat');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 const paths = require('../lib/paths');
 const onStreamError = require('../lib/on-stream-error');
 
-gulp.src(['src/components/**/*.js', 'src/index.js'])
+gulp.src(['src/components/**/*.js'])
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(concat('index.js'))
+    .pipe(babel({
+        presets: ['es2015']
+    }))
     .pipe(uglify())
     .on('error', onStreamError)
     .pipe(sourcemaps.write('./'))
