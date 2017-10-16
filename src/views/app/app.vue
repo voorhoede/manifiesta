0f45585af695f4ba24e092bf859783e9841f44a1
Casper Boutens <casper@voorhoede.nl>
2017-10-16 16:50:09 +0200

    fix(structure): rename code-editor to manifest-editor

--- src/views/app/app.vue
+++ src/views/app/app.vue
@@ -2,20 +2,20 @@
   <div class="app">
     <div class="input-container">
       <settings></settings>
-      <code-editor :setManifest="setManifest"></code-editor>
+      <manifest-editor :setManifest="setManifest"></manifest-editor>
     </div>
     <preview class="output-container" :manifest="manifest"></preview>
   </div>
 </template>
 
 <script>
-  import CodeEditor from '../../components/code-editor/code-editor'
+  import ManifestEditor from '../../components/manifest-editor/manifest-editor'
   import Settings from '../../components/settings/settings'
   import Preview from '../../components/preview/preview'
 
   export default {
     components: {
-      CodeEditor,
+      ManifestEditor,
       Settings,
       Preview
     },
