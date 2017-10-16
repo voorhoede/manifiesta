73a1308ce8458f1b3f7b1294c9c4da5e93bfb6ab
Casper Boutens <casper@voorhoede.nl>
2017-10-16 16:55:23 +0200

    fix(structure): move views to components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './views/app/app'
+import App from './components/app/app'
 
 Vue.config.productionTip = false
 
