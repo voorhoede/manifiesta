e2d308ee1b8a8b8de2b7b737dc6a93b6ab2c5895
Casper Boutens <casper@voorhoede.nl>
2017-10-16 10:06:36 +0200

    feat(push-notification): create better close button html, style

--- src/components/push-notification/push-notification.vue
+++ src/components/push-notification/push-notification.vue
@@ -8,7 +8,11 @@
         <h2 class="push-notification-title">{{ shortName }}</h2>
         <p class="push-notification-url">{{ removeHost }}</p>
       </div>
-      <button class="push-notification-close">&times;</button>
+      <button class="push-notification-close">
+        <svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 0 14 14" width="14">
+          <path d="M14 1.4L12.6 0 7 5.6 1.4 0 0 1.4 5.6 7 0 12.6 1.4 14 7 8.4l5.6 5.6 1.4-1.4L8.4 7 14 1.4z" fill-rule="evenodd"/>
+        </svg>
+      </button>
     </div>
     <button type="button" class="push-notification-open">Toevoegen</button>
   </div>
