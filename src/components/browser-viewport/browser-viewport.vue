6aec19b995da04ff40099c464199d9ac44c4d3b1
Jasper Moelker <j.b.moelker@activeids.nl>
2017-10-23 20:58:59 +0200

    Use image in viewport if X-Frame-Options is set

--- src/components/browser-viewport/browser-viewport.vue
+++ src/components/browser-viewport/browser-viewport.vue
@@ -1,7 +1,7 @@
 <template>
-  <iframe :src="url" class="browser-viewport">
-    <p>Your browser does not support iframes.</p>
-  </iframe>
+    <iframe v-if="isFrameable" :src="url" class="browser-viewport"></iframe>
+    <img v-else-if="imageUrl" :alt="`Screenshot of ${url}`" :src="imageUrl" class="browser-viewport">
+    <div v-else class="browser-viewport"><p>Site doesn't allow embedding. Fetching as image...</p></div>
 </template>
 
 <script>
@@ -10,6 +10,59 @@
       url: {
         type: String,
         required: true
+      }
+    },
+    data () {
+      return {
+        imageUrl: undefined,
+        isFrameable: true
+      }
+    },
+    mounted: function () {
+      this.checkFrameOptions(this.url)
+    },
+    methods: {
+      checkFrameOptions: function (url) {
+        this.isFrameable = true
+        fetch(`${process.env.FETCH_HEADERS_API}?url=${url}`)
+          .then(response => response.json())
+          .then(({ errors, headers = [] }) => {
+            if (errors) {
+              return Promise.reject(errors[9])
+            }
+            const xFrameOptions = headers['x-frame-options']
+            if (xFrameOptions) {
+              this.isFrameable = false
+              this.fetchScreenshot(url)
+            } else {
+              this.isFrameable = true
+            }
+          })
+      },
+      fetchScreenshot: function (url) {
+        const dimensions = this.$el.getBoundingClientRect()
+        const dpr = window.devicePixelRatio
+        const width = Math.ceil(dimensions.width * dpr)
+        const height = Math.ceil(dimensions.height * dpr)
+        this.imageUrl = undefined
+        fetch(`${process.env.WEBSHOT_API}?url=${url}&w=${width}&h=${height}`)
+          .then(response => {
+            if (response.ok) {
+              return response.blob()
+            } else {
+              return response.json()
+                .then(json => Promise.reject(json.errors[0]))
+            }
+          })
+          .then(imageBlob => {
+            this.imageUrl = URL.createObjectURL(imageBlob)
+          })
+          .catch(error => console.error(error.message))
+      }
+    },
+    watch: {
+      url: function (url) {
+        this.checkFrameOptions(url)
       }
     }
   }
