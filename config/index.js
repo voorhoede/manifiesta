685ebbb8b5416b4eefc0acdbc81e40e8cf4c317c
Casper Boutens <casper@voorhoede.nl>
2017-10-24 16:46:30 +0200

    fix(config): change to project_name

--- config/index.js
+++ config/index.js
@@ -3,14 +3,15 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path')
+const {PROJECT_NAME, ...prodEnv} = require('./prod.env')
 
 module.exports = {
   build: {
-    env: require('./prod.env'),
+    env: prodEnv,
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: `./`,
+    assetsPublicPath: `${PROJECT_NAME}/`,
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
--- config/prod.env.js
+++ config/prod.env.js
@@ -3,5 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   FETCH_HEADERS_API: `"https://fetch-headers.now.sh/"`,
   FETCH_MANIFEST_API: `"https://fetch-manifest.now.sh/"`,
-  WEBSHOT_API: `"https://webshot.now.sh/"`
+  WEBSHOT_API: `"https://webshot.now.sh/"`,
+  PROJECT_NAME: "manifiesta"
 }
