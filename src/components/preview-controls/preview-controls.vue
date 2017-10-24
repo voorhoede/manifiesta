0e5c2a0bf04adedc098954c2b5514a7d5b84f5e2
Casper Boutens <casper@voorhoede.nl>
2017-10-24 13:37:01 +0200

    feat(preview-controls): add 'correct' style

--- src/components/preview-controls/preview-controls.vue
+++ src/components/preview-controls/preview-controls.vue
@@ -1,46 +1,35 @@
 <template>
-  <v-card class="preview-controls">
-    <v-card-text>
-
-      <v-btn-toggle v-model="toggleView" mandatory>
-        <v-btn flat>
-          <v-icon left>mdi-view-column</v-icon>
-          Side-by-side
-        </v-btn>
-        <v-btn flat>
-          Flow
-          <v-icon right>mdi-view-array</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-
-      <v-select v-bind:items="browsers" v-model="browser" label="Browser" single-line bottom
-      ></v-select>
-
-      <div class="preview-controls-slider">
-        Scale: {{computedZoomLevel}}
-        <v-slider v-model="zoomLevel" v-bind:max="zoomLevelMax" step="10" snap></v-slider>
-      </div>
-
-      <v-btn-toggle v-model="orientation" mandatory>
-        <v-btn flat>
-          <v-icon left>mdi-cellphone</v-icon>
-          Portrait
-        </v-btn>
-        <v-btn flat>
-          Landscape
-          <v-icon right>mdi-tablet</v-icon>
-        </v-btn>
-      </v-btn-toggle>
-
-    </v-card-text>
-  </v-card>
+  <v-toolbar class="preview-controls white elevation-1">
+    <v-select disabled :items="browsers" v-model="browser" class="preview-controls-select mr-4"></v-select>
+    <v-slider disabled class="preview-controls-slider mr-4" v-model="zoomLevel" :min="zoomLevelMin" :max="zoomLevelMax" :step="zoomStep" snap :label="computedZoomLevel"></v-slider>
+    <v-btn-toggle v-model="toggleView" mandatory class="mr-4">
+      <v-btn flat disabled>
+        <v-icon left >mdi-view-column</v-icon>
+        Side-by-side
+      </v-btn>
+      <v-btn flat disabled>
+        Flow
+        <v-icon right>mdi-view-array</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+    <v-btn-toggle v-model="orientation" mandatory class="mr-4">
+      <v-btn flat disabled>
+        <v-icon left>mdi-cellphone</v-icon>
+        Portrait
+      </v-btn>
+      <v-btn flat disabled>
+        Landscape
+        <v-icon right>mdi-tablet</v-icon>
+      </v-btn>
+    </v-btn-toggle>
+  </v-toolbar>
 </template>
 
 <script>
 export default {
   computed: {
     computedZoomLevel () {
-      return this.zoomLevel / 100
+      return (this.zoomLevel / 100).toFixed(1)
     }
   },
   data () {
@@ -55,6 +44,8 @@ export default {
         'Opera'
       ],
       zoomLevel: 100,
+      zoomStep: 10,
+      zoomLevelMin: 10,
       zoomLevelMax: 200,
       orientation: 0
     }
