b72ec8d9cf2c89450bb240052865c89d81d6e92c
Jeroen D <Ex-iT@users.noreply.github.com>
2017-11-20 10:03:05 +0100

    fix(app-header): simplified toggle button (#31)

--- src/components/app-header/app-header.vue
+++ src/components/app-header/app-header.vue
@@ -1,10 +1,8 @@
 <template>
   <v-toolbar clipped-left absolute app color="primary">
-    <v-btn v-if="this.isOpen" icon class="white--text" @click.stop="toggleEditor">
-      <v-icon>arrow_back</v-icon>
-    </v-btn>
-    <v-btn v-if="!this.isOpen" icon class="white--text" @click.stop="toggleEditor">
-      <v-icon>arrow_forward</v-icon>
+    <v-btn icon class="white--text" @click.stop="toggleEditor">
+      <v-icon v-if="this.isOpen">arrow_back</v-icon>
+      <v-icon v-if="!this.isOpen">arrow_forward</v-icon>
     </v-btn>
     <v-toolbar-title class="white--text">Manifiesta</v-toolbar-title>
   </v-toolbar>
