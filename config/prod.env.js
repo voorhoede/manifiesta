77f8aa55070f33f08d0eb1d112d1387f31cef15d
Jasper Moelker <j.b.moelker@activeids.nl>
2018-03-25 13:45:21 +0200

    Use fetch-headers Netlify function in production

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  FETCH_HEADERS_API: `"https://fetch-headers.now.sh/"`,
+  FETCH_HEADERS_API: `"/.netlify/functions/fetch-headers/"`,
   FETCH_MANIFEST_API: `"https://fetch-manifest.now.sh/"`,
   WEBSHOT_API: `"https://webshot.now.sh/"`
 }
